dc5a395a9ec14b4bfda8cc7b53d19140fdb22d02
Irit41 <81565397+Irit41@users.noreply.github.com>
2022-12-18 22:24:03 +0200

    18/12

--- app/src/main/java/com/example/tefillion/MainActivity.java
+++ app/src/main/java/com/example/tefillion/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.tefillion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +20,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("TAG", "Fetching FCM registration token failed");
+                            return;
+                        }
 
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+//                        Log.d("TAGuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuutoken ", token);
+//                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 Intent home = new Intent(MainActivity.this,home_page.class);
                 startActivity(home);
                 finish();
